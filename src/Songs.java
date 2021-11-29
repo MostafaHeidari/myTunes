58daf52611ba9f6018f52f51b96fab37f27aad30
Math376i <91528384+Math376i@users.noreply.github.com>
2021-11-29 13:24:51 +0100

    Update Songs.java
    Songs maked

--- src/Songs.java
+++ src/Songs.java
@@ -1,2 +1,52 @@
 public class Songs {
+
+
+    private final String artist;
+    private String title;
+    private String Artist;
+    private String categori;
+    private int time;
+
+    public Songs(String title, String Artist, String artist, String categori, int time)
+    {
+        this.title = title;
+        this.artist = artist;
+        this.Artist = this.artist;
+        this.categori= categori;
+        this.time= time;
+    }
+
+    public String getTitle()
+    {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        Artist = artist;
+    }
+
+    public String getCategori() {
+        return categori;
+    }
+
+    public void setCategori(String categori) {
+        this.categori = categori;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public void setTime(int time) {
+        this.time = time;
+    }
 }
+
