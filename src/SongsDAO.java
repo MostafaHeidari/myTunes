13e970c19b4205b4df9bf11a7effaef2e4ae654a
Math376i <91528384+Math376i@users.noreply.github.com>
2021-11-29 13:49:07 +0100

    Create SongsDAO.java

---
+++ src/SongsDAO.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class SongsDAO< AllSongs> {
+
+    private Object AllSongs;
+    private Object Artist;
+
+    public List<Songs> getAllsongs() throws FileNotFoundException, IOException {
+        List<Songs> allSongs = new ArrayList<>();
+
+        return (List<Songs>) AllSongs;
+    }
+
+
+
+}
