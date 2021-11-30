3eff7cf19dceeda9704a50c318ab30f4d9eabece
Math376i <91528384+Math376i@users.noreply.github.com>
2021-11-30 18:00:06 +0100

    Update MyTunesController.java

--- src/MyTunes/controller/MyTunesController.java
+++ src/MyTunes/controller/MyTunesController.java
@@ -16,6 +16,44 @@ public class MyTunesController implements Initializable {
     public void handleMoveSongToPL(ActionEvent actionEvent) {
     }
 
+   
+
+    public void handleDeletePlaylist(ActionEvent actionEvent) {
+    }
+
+    public void handleNewPlaylist(ActionEvent actionEvent) {
+    }
+
+    public void handleEditPlaylist(ActionEvent actionEvent) {
+    }
+
+    public void handleMoveSongUp(ActionEvent actionEvent) {
+    }
+
+    public void handleMoveSongDown(ActionEvent actionEvent) {
+    }
+
+    public void handleCloseApp(ActionEvent actionEvent) {
+    }
+
+    public void handleNewSong(ActionEvent actionEvent) {
+    }
+
+    public void handleDeleteSong(ActionEvent actionEvent) {
+    }
+
+    public void handleEditSong(ActionEvent actionEvent) {
+    }
+
+    public void handleMusicPlayPause(ActionEvent actionEvent) {
+    }
+
     public void handleDeleteSongOnPL(ActionEvent actionEvent) {
+    }
+
+    public void handleMusicForward(ActionEvent actionEvent) {
+    }
+
+    public void handleMusicRewind(ActionEvent actionEvent) {
     }
 }
