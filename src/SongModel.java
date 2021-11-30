f4a671ad51801285a11ecdb327bb71576ff07a02
Math376i <91528384+Math376i@users.noreply.github.com>
2021-11-30 10:24:39 +0100

    Update SongModel.java

--- src/SongModel.java
+++ src/SongModel.java
@@ -1,7 +1,7 @@
 public class SongModel<MRSLogicFacade> {
     private MRSLogicFacade logiclayer;
 
-
+/*
  /*   public Songs createMovie(String title, String artist, String categori, int time) {
         SongModel logiclayer = null;
         Songs song = logiclayer.createMovie(title,artist,categori,time);
