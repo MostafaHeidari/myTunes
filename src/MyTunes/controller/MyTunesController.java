fe097f5600ababb830a9e0ced204b4cc9e3dc990
AndersJakobsen <ande12n3@easv365.dk>
2021-12-01 09:32:16 +0100

    Update MyTunesController.java

--- src/MyTunes/controller/MyTunesController.java
+++ src/MyTunes/controller/MyTunesController.java
@@ -2,11 +2,14 @@ package MyTunes.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MyTunesController implements Initializable {
+
+    public Button newSong;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
