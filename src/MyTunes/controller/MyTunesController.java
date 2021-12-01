856ec6a8103f7413f36521a133838fb345fb4a08
Math376i <91528384+Math376i@users.noreply.github.com>
2021-12-01 09:32:12 +0100

    Update MyTunesController.java

--- src/MyTunes/controller/MyTunesController.java
+++ src/MyTunes/controller/MyTunesController.java
@@ -2,20 +2,19 @@ package MyTunes.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableView;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MyTunesController implements Initializable {
 
+    public TableView tableOfSongs;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
     }
 
-    public void handleMoveSongToPL(ActionEvent actionEvent) {
-    }
 
-    public void handleDeleteSongOnPL(ActionEvent actionEvent) {
-    }
 }
