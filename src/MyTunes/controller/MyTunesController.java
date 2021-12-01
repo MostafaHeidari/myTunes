0aefbef9789ffdd4bcaaf13445d48a06ff7e542f
Math376i <91528384+Math376i@users.noreply.github.com>
2021-12-01 09:45:24 +0100

    Update MyTunesController.java

--- src/MyTunes/controller/MyTunesController.java
+++ src/MyTunes/controller/MyTunesController.java
@@ -5,7 +5,9 @@ import javafx.fxml.Initializable;
 
 import javafx.scene.control.Button;
 
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.image.ImageView;
 
 
 import java.net.URL;
@@ -17,6 +19,13 @@ public class MyTunesController implements Initializable {
     public Button newSong;
 
     public TableView tableOfSongs;
+    public TableView SongsOnPlaylist;
+    public TableView TabelOfPlaylist;
+    public TableColumn songTitleCol;
+    public TableColumn songArtistCol;
+    public TableColumn songCatCol;
+    public TableColumn songDurationCol;
+    public ImageView musicRewind;
 
 
     @Override
