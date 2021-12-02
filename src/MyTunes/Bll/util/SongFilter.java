5057c136df3633a4194668ae334c2c93c2149972
Math376i <91528384+Math376i@users.noreply.github.com>
2021-12-02 10:29:56 +0100

    Create SongFilter.java

---
+++ src/MyTunes/Bll/util/SongFilter.java
@@ -0,0 +1,9 @@
+package MyTunes.Bll.util;
+
+import MyTunes.be.Songs;
+import javafx.collections.ObservableList;
+
+public class SongFilter {
+    public void searchSong(ObservableList<Songs> songs, String text) {
+    }
+}
