9394d7176f4d7adf980a5228b4c0c089a2b6d085
Math376i <91528384+Math376i@users.noreply.github.com>
2021-12-02 10:29:52 +0100

    Update SongManager.java

--- src/MyTunes/Bll/SongManager.java
+++ src/MyTunes/Bll/SongManager.java
@@ -1,4 +1,45 @@
 package MyTunes.Bll;
 
-public class SongManager {
+import MyTunes.Bll.util.SongFilter;
+import MyTunes.Dal.SongsDAO;
+import MyTunes.be.Songs;
+import javafx.collections.ObservableList;
+
+import java.io.IOException;
+import java.util.List;
+
+public class SongManager
+{
+    private SongsDAO songsDAO;
+    private SongFilter songFilter;
+
+
+    public SongManager() throws IOException{
+        songsDAO = new SongsDAO();
+        songFilter = new SongFilter();
+    }
+
+
+
+
+    public Songs addSong(String title, String artist, String genre, int playtime, String location, int id) {
+        return songsDAO.addSong(title, artist, genre, playtime, location);
+    }
+
+    public Songs updateSong(Songs song, String title, String artist, String genre, int playtime, String location) {
+        return songsDAO.updateSong(song,title,artist,genre,playtime,location);
+    }
+
+    public Songs deleteSong(Songs songDelete) {
+        return songsDAO.deleteSong(songDelete);
+    }
+
+
+    public List <Songs> getSongs() {
+        List<Songs> allSongs = (List<Songs>) songsDAO.getSongs();
+        return allSongs;
+    }
+    public ObservableList<Songs> searchSongs(ObservableList<Songs> songs, String text) {
+        return songFilter.searchSong(songs,text);
+    }
 }
