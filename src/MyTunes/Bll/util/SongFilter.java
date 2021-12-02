45a07a9282dc0af378b04d38b27e98c9f71966ea
Math376i <91528384+Math376i@users.noreply.github.com>
2021-12-02 10:55:18 +0100

    Update SongFilter.java

--- src/MyTunes/Bll/util/SongFilter.java
+++ src/MyTunes/Bll/util/SongFilter.java
@@ -1,9 +1,21 @@
 package MyTunes.Bll.util;
 
 import MyTunes.be.Songs;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
 public class SongFilter {
-    public void searchSong(ObservableList<Songs> songs, String text) {
+    private ObservableList<Songs>songSearch = FXCollections.observableArrayList();
+
+
+    public ObservableList<Songs> searchSong(ObservableList<Songs> songs, String text) {
+        songSearch.clear();
+        for( Songs Songs : songs);{
+
+            if(Songs.getTitle().toLowerCase().startsWith(text.toLowerCase())){
+                songSearch.add((Songs) songs);
+            }
+        }
+        return songSearch;
     }
 }
