dfb38bbaa941bfbeac3ca99248cded45c1c115d5
Math376i <91528384+Math376i@users.noreply.github.com>
2021-12-02 10:43:34 +0100

    Update NewPlaylist.java

--- src/MyTunes/be/NewPlaylist.java
+++ src/MyTunes/be/NewPlaylist.java
@@ -1,6 +1,42 @@
 package MyTunes.be;
 
+import javafx.scene.Parent;
+
+import java.util.List;
+
 public class NewPlaylist {
 
+    private List<Songs> playlist;
+    private String playlistName;
+    private int ID;
 
+    public NewPlaylist(int id,String name){
+        this.playlistName = name;
+        this.ID = ID;
+    }
+
+    public List<Songs>getPlaylist(){
+        return playlist;
+    }
+
+    public void setPlaylist(List<Songs> playlist) {
+        this.playlist = playlist;
+    }
+
+    public String getPlaylistName(String playlistName)
+    {
+        return playlistName;
+    }
+
+    public void setPlaylistName(String playlistName) {
+        this.playlistName = playlistName;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
 }
