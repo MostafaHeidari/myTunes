b4bb050bfa8ac934a512db4033f9eef45f2c8729
AndersJakobsen <ande12n3@easv365.dk>
2021-12-05 15:31:05 +0100

    img files

--- src/MyTunes/gui/controller/PlaylistController.java
+++ src/MyTunes/gui/controller/PlaylistController.java
@@ -1,19 +1,39 @@
-package MyTunes.gui.controller;
+package sample.gui.model;
 
-import MyTunes.gui.model.SongModel;
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.media.MediaPlayer;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import MyTunes.be.NewPlaylist;
+import MyTunes.be.Songs;
+import MyTunes.Bll.PlaylistManager;
 
-import java.awt.*;
 import java.io.IOException;
-import java.net.URL;
-import java.util.ResourceBundle;
 
-public class PlaylistController  {
+public class PlaylistModel {
 
+    private ObservableList<NewPlaylist> allPlaylists;
 
+    private final PlaylistManager playlistManager;
 
+    public PlaylistModel() throws IOException {
+        playlistManager = new PlaylistManager();
+    }
 
+    /**
+     * Method for getting the playlists
+     * @return
+     */
+    public ObservableList<NewPlaylist> getAllPlaylists() {
+        allPlaylists = FXCollections.observableArrayList();
+        allPlaylists.addAll(playlistManager.getAllPlaylists());
+        return allPlaylists;
+    }
+
+    /**
+     * Method to create a new playlist
+     * @param name
+     */
+    public void createPlaylist (String name) {
+        playlistManager.createPlaylist(name);
+    }
 
 }
