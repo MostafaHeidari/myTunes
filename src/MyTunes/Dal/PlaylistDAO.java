b210a5b6d276c113cdcf66b81140b380e7c25ff0
Math376i <91528384+Math376i@users.noreply.github.com>
2021-12-06 10:56:31 +0100

    Update PlaylistDAO.java

--- src/MyTunes/Dal/PlaylistDAO.java
+++ src/MyTunes/Dal/PlaylistDAO.java
@@ -1,4 +1,26 @@
 package MyTunes.Dal;
 
+import MyTunes.be.NewPlaylist;
+
+import java.util.List;
+
 public class PlaylistDAO {
+
+import java.util.List;
+
+    public class PlaylistDao {
+        public String createPlaylist(String name) {
+
+
+            public List<NewPlaylist> getAllPlaylists() {
+
+                public NewPlaylist createPlaylist(String name)
+            {
+
+            }
+
+            }
+
+        }
+    }
 }
