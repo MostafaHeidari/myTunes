d035aa866d4abfe13cddc7d0e76ae090d28f295f
Math376i <91528384+Math376i@users.noreply.github.com>
2021-12-06 09:12:14 +0100

    Create Metadata.java

---
+++ src/MyTunes/gui/controller/Metadata.java
@@ -0,0 +1,11 @@
+package MyTunes.gui.controller;
+
+public class Metadata {
+    public String[] names() {
+        return names();
+    }
+
+    public String get(String title) {
+        return title;
+    }
+}
