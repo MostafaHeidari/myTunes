0cfa04348fe6248098305b74c6ea591121192890
MostafaHeidari <70130828+MostafaHeidari@users.noreply.github.com>
2021-12-14 21:20:44 +0100

    vi kan spille song nu, jaaa

--- src/MyTunes/gui/controller/PlaylistController.java
+++ src/MyTunes/gui/controller/PlaylistController.java
@@ -9,7 +9,6 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
